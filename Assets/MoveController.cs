5a5f7f762ef4080599cd6fa996b7d3d051753c1a
tsagara <tsagara@aiming-inc.com>
2017-01-19 18:56:30 +0900

    移動と色変え

---
+++ Assets/MoveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MoveController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.isLocalPlayer && Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime);
+		
+	}
+}
---
+++ Assets/NetworkColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkColor : NetworkBehaviour {
+    [SyncVar]
+    private int colorIndex = 0;
+    Color[] colorList = {
+        Color.red,
+        Color.blue,
+        Color.yellow
+    };
+	
+	// Update is called once per frame
+    [ClientCallback]
+	void Update () {
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdChangeColor();
+        }
+
+        var renderer = this.GetComponent<Renderer>();
+        renderer.material.color = colorList[colorIndex % colorList.Length];
+	}
+
+    [Command]
+    void CmdChangeColor()
+    {
+        colorIndex++;
+    }
+}
