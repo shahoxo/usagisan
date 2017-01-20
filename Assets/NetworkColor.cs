d587dd814fd7bca5a47b420e3e3e9a9221ff4f15
tsagara <tsagara@aiming-inc.com>
2017-01-20 15:57:22 +0900

    UNETのメッセージ機能をつかって色を変えた（バグ有

--- Assets/NetworkColor.cs
+++ Assets/NetworkColor.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+public static class UsagisanMessageType
+{
+    // MsgTypeが拡張できるならそっちでやる。
+    public static readonly short ChangeColor = MsgType.Highest + 1;
+}
+
 public class NetworkColor : NetworkBehaviour {
     [SyncVar]
     private int colorIndex = 0;
@@ -11,22 +17,45 @@ public class NetworkColor : NetworkBehaviour {
         Color.blue,
         Color.yellow
     };
+
+    class ColorMessage : MessageBase
+    {
+        public Color color;
+    }
+
+
+    void Start()
+    {
+        
+        NetworkManager.singleton.client.RegisterHandler(UsagisanMessageType.ChangeColor, (netMsg) =>
+        {
+            ColorMessage colorMessage = netMsg.ReadMessage<ColorMessage>();
+            var renderer = this.GetComponent<Renderer>();
+            renderer.material.color = colorMessage.color;
+        });
+    }
 	
-	// Update is called once per frame
-    [ClientCallback]
+	// Update is called once per frame//[ClientCallback]
 	void Update () {
         if (isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
         {
-            CmdChangeColor();
+            //CmdChangeColor();
+            ChangeColorMessage();
         }
-
-        var renderer = this.GetComponent<Renderer>();
-        renderer.material.color = colorList[colorIndex % colorList.Length];
 	}
 
     [Command]
     void CmdChangeColor()
     {
         colorIndex++;
+    }
+
+    void ChangeColorMessage()
+    {
+        colorIndex++;
+        ColorMessage message = new ColorMessage();
+        message.color = colorList[colorIndex % 3];
+
+        NetworkServer.SendToAll(UsagisanMessageType.ChangeColor, message);
     }
 }
